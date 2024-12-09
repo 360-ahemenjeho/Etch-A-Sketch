b413a11df536a7359ecc4c8e7a27e1fffb8e6835
360-ahemenjeho <ahemenjeho2312@outlook.com>
2024-12-09 01:40:22 +0100

    feat: create square grid

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-window.alert('Are you ready?')
+const mainContainerEl = document.querySelector('#mainContainer')
+
+for (let i = 0; i < 16; i++) {
+  const containerEl = document.createElement('div')
+  containerEl.classList.add('container')
+  for (let j = 0; j < 16; j++) {
+    const squareEl = document.createElement('div')
+    squareEl.classList.add('square')
+
+    containerEl.appendChild(squareEl)
+  }
+  mainContainerEl.appendChild(containerEl)
+}
